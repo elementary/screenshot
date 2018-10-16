69dc25bbfaddec01148662df7965bb8beabbab68
worldofpeace <worldofpeace@users.noreply.github.com>
2018-10-16 04:50:02 -0400

    Include Redacted Script Font

--- meson/post_install.py
+++ meson/post_install.py
@@ -2,9 +2,14 @@
 
 import os
 import subprocess
-
-schemadir = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0', 'schemas')
+import sys
 
 if not os.environ.get('DESTDIR'):
-	print('Compiling gsettings schemas...')
-	subprocess.call(['glib-compile-schemas', schemadir])
+    print('Compiling gsettings schemas...')
+    subprocess.call(['glib-compile-schemas', os.path.join(sys.argv[1], 'glib-2.0', 'schemas')], shell=False)
+
+    print('Rebuilding desktop icons cache...')
+    subprocess.call(['gtk-update-icon-cache', os.path.join(sys.argv[1], 'icons', 'hicolor')], shell=False)
+
+    print('Rebuilding font cache...')
+    subprocess.call(['fc-cache', '-f', os.path.join(sys.argv[1], 'fonts', 'truetype', 'redacted-elementary')], shell=True)
